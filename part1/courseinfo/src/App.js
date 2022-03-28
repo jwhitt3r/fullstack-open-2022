4312f80f12305d05b372ce56428fbdb821f4046b
jwhitter <jackwhitterjones@gmail.com>
2022-03-28 13:17:49 +0100

    Part 1.5 - Added the constant definitions into a single object and fixed the application

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,14 +1,14 @@
 
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
   )
 }
 
 const Content = (props) => {
   return(
     <>
-      {props.parts.map(part => <Part part={part.name} exercises={part.exercises} />)}
+      {props.parts.map((part) => <Part part={part.name} exercises={part.exercises} />)}
     </>
   )
 }
@@ -29,27 +29,30 @@ const Part = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7
-  },
-  {
-    name: 'State of a component',
-    exercises: 14
-  }
-]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+}
+
 
   return (
     <div>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total parts={parts} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
   )
 }
